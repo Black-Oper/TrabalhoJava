28322ccdae35962a898341c7f61bfde186c9b2b6
hooltz <146349132+hooltz@users.noreply.github.com>
2024-06-23 22:24:42 -0300

    add arbustos, montanha

--- src/Tabuleiro.java
+++ src/Tabuleiro.java
@@ -74,6 +74,434 @@ public class Tabuleiro {
         }
     }
 
+    for (int i = 81; i <= 85; i++) {
+        for (int j = 66; j <= 67; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 79; i <= 80; i++) {
+        for (int j = 61; j <= 65; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 74; i <= 75; i++) {
+        for (int j = 63; j <= 65; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    tabuleiro[76][64] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    tabuleiro[76][65] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+
+    tabuleiro[83][61] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    tabuleiro[84][61] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    tabuleiro[85][61] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    tabuleiro[84][62] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    tabuleiro[85][62] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    tabuleiro[85][63] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+
+    for (int i = 76; i <= 77; i++) {
+        for (int j = 68; j <= 71; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 74; i <= 75; i++) {
+        for (int j = 70; j <= 71; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 70; i <= 71; i++) {
+        for (int j = 68; j <= 71; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    tabuleiro[72][68] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    tabuleiro[72][69] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    tabuleiro[72][70] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+
+    for (int i = 66; i <= 71; i++) {
+        for (int j = 63; j <= 65; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 69; i <= 71; i++) {
+        for (int j = 53; j <= 60; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 66; i <= 68; i++) {
+        for (int j = 58; j <= 60; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    tabuleiro[68][53] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    tabuleiro[68][54] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    tabuleiro[68][55] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+
+    for (int i = 72; i <= 75; i++) {
+        for (int j = 56; j <= 57; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 76; i <= 77; i++) {
+        for (int j = 55; j <= 58; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 81; i <= 82; i++) {
+        for (int j = 53; j <= 54; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    tabuleiro[82][55] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+
+    for (int i = 85; i <= 86; i++) {
+        for (int j = 53; j <= 58; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    //arbustos - cidade2
+
+    for (int i = 84; i <= 86; i++) {
+        for (int j = 45; j <= 47; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 78; i <= 79; i++) {
+        for (int j = 46; j <= 48; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    tabuleiro[77][47] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    tabuleiro[77][48] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    tabuleiro[77][49] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    tabuleiro[78][49] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+
+    for (int i = 91; i <= 93; i++) {
+        for (int j = 37; j <= 40; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+
+        for (int j = 28; j <= 32; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 83; i <= 86; i++) {
+        for (int j = 23; j <= 25; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 81; i <= 90; i++) {
+        for (int j = 7; j <= 9; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 81; i <= 83; i++) {
+        for (int j = 10; j <= 12; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 73; i <= 76; i++) {
+        for (int j = 15; j <= 17; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 71; i <= 72; i++) {
+        for (int j = 9; j <= 12; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 70; i <= 73; i++) {
+        for (int j = 5; j <= 7; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    tabuleiro[69][6] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    tabuleiro[69][7] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+
+    tabuleiro[76][11] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    tabuleiro[76][12] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+
+    for (int i = 62; i <= 63; i++) {
+        for (int j = 5; j <= 7; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 64; i <= 66; i++) {
+        for (int j = 5; j <= 10; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int j = 6; j <= 10; j++) {
+        tabuleiro[67][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    }
+
+    for (int i = 57; i <= 58; i++) {
+        for (int j = 7; j <= 10; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 57; i <= 67; i++) {
+        for (int j = 12; j <= 13; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 57; i <= 62; i++) {
+        for (int j = 16; j <= 19; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 66; i <= 67; i++) {
+        for (int j = 16; j <= 22; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int j = 19; j <= 22; j++) {
+        tabuleiro[68][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    }
+
+    for (int i = 69; i <= 70; i++) {
+        for (int j = 19; j <= 20; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    //arbustos - cidade3
+
+    for (int i = 48; i <= 49; i++) {
+        for (int j = 9; j <= 11; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 53; i <= 54; i++) {
+        for (int j = 17; j <= 25; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 48; i <= 49; i++) {
+        for (int j = 21; j <= 24; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    tabuleiro[47][21] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    tabuleiro[47][22] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+
+    for (int i = 41; i <= 44; i++) {
+        for (int j = 18; j <= 19; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    tabuleiro[43][20] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    tabuleiro[44][20] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+
+    for (int i = 39; i <= 40; i++) {
+        for (int j = 32; j <= 34; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+        for (int j = 39; j <= 40; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 44; i <= 45; i++) {
+        for (int j = 32; j <= 35; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 41; i <= 42; i++) {
+        for (int j = 38; j <= 40; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 43; i <= 45; i++) {
+        for (int j = 43; j <= 47; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 39; i <= 41; i++) {
+        for (int j = 48; j <= 49; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 42; i <= 44; i++) {
+        for (int j = 50; j <= 51; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    //arbustos - cidade4
+
+    for (int i = 43; i <= 46; i++) {
+        for (int j = 56; j <= 57; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 49; i <= 53; i++) {
+        for (int j = 53; j <= 54; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 50; i <= 52; i++) {
+        for (int j = 57; j <= 59; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 53; i <= 54; i++) {
+        for (int j = 44; j <= 46; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int j = 44; j <= 45; j++) {
+        tabuleiro[55][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    }
+
+    for (int j = 35; j <= 39; j++) {
+        tabuleiro[48][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    }
+
+    for (int j = 49; j <= 54; j++) {
+        tabuleiro[j][35] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    }
+
+    for (int i = 66; i <= 68; i++) {
+        for (int j = 47; j <= 50; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 61; i <= 62; i++) {
+        for (int j = 63; j <= 65; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 59; i <= 61; i++) {
+        for (int j = 69; j <= 71; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    tabuleiro[61][62] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    tabuleiro[59][72] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+    tabuleiro[60][72] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+
+    for (int i = 57; i <= 58; i++) {
+        for (int j = 76; j <= 79; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 55; i <= 56; i++) {
+        for (int j = 78; j <= 79; j++) {
+            tabuleiro[i][j] = new Grama("\u001B[42m"  + "🌾" + "\u001B[0m");
+        }
+    }
+
+    //rio1
+
+    for (int i = 86; i <= 87; i++) {
+        for (int j = 30; j <= 35; j++) {
+            tabuleiro[i][j] = new Parede("\u001B[44m" + "🟦" + "\u001B[0m");
+        }
+    }
+
+    for (int j = 31; j <= 34; j++) {
+        tabuleiro[85][j] = new Parede("\u001B[44m" + "🟦" + "\u001B[0m");
+    }
+
+    for (int j = 31; j <= 38; j++) {
+        tabuleiro[88][j] = new Parede("\u001B[44m" + "🟦" + "\u001B[0m");
+    }
+
+    for (int j = 34; j <= 38; j++) {
+        tabuleiro[89][j] = new Parede("\u001B[44m" + "🟦" + "\u001B[0m");
+    }
+
+    tabuleiro[87][36] = new Parede("\u001B[44m" + "🟦" + "\u001B[0m");
+    tabuleiro[87][37] = new Parede("\u001B[44m" + "🟦" + "\u001B[0m");
+
+    //rio2
+
+    for (int j = 63; j <= 64; j++) {
+        tabuleiro[53][j] = new Parede("\u001B[44m" + "🟦" + "\u001B[0m");
+    }
+    for (int j = 63; j <= 64; j++) {
+        tabuleiro[54][j] = new Parede("\u001B[44m" + "🟦" + "\u001B[0m");
+    }
+    for (int j = 63; j <= 64; j++) {
+        tabuleiro[55][j] = new Parede("\u001B[44m" + "🟦" + "\u001B[0m");
+    }
+    for (int j = 61; j <= 64; j++) {
+        tabuleiro[56][j] = new Parede("\u001B[44m" + "🟦" + "\u001B[0m");
+    }
+    for (int j = 60; j <= 63; j++) {
+        tabuleiro[58][j] = new Parede("\u001B[44m" + "🟦" + "\u001B[0m");
+    }
+    for (int j = 58; j <= 62; j++) {
+        tabuleiro[59][j] = new Parede("\u001B[44m" + "🟦" + "\u001B[0m");
+    }
+    for (int j = 48; j <= 60; j++) {
+        tabuleiro[60][j] = new Parede("\u001B[44m" + "🟦" + "\u001B[0m");
+    }
+    for (int j = 43; j <= 59; j++) {
+        tabuleiro[61][j] = new Parede("\u001B[44m" + "🟦" + "\u001B[0m");
+    }
+    for (int j = 42; j <= 50; j++) {
+        tabuleiro[62][j] = new Parede("\u001B[44m" + "🟦" + "\u001B[0m");
+    }
+    for (int j = 40; j <= 46; j++) {
+        tabuleiro[63][j] = new Parede("\u001B[44m" + "🟦" + "\u001B[0m");
+    }
+    for (int j = 39; j <= 45; j++) {
+        tabuleiro[64][j] = new Parede("\u001B[44m" + "🟦" + "\u001B[0m");
+    }
+    for (int j = 38; j <= 44; j++) {
+        tabuleiro[65][j] = new Parede("\u001B[44m" + "🟦" + "\u001B[0m");
+    }
+    for (int j = 38; j <= 43; j++) {
+        tabuleiro[66][j] = new Parede("\u001B[44m" + "🟦" + "\u001B[0m");
+    }
+    
+
     //casa1 - cidade1
     tabuleiro[89][87] = new Parede("\u001B[47m"  + "⬜" + "\u001B[0m");
     tabuleiro[89][86] = new Parede("\u001B[47m"  + "⬜" + "\u001B[0m");
@@ -357,6 +785,19 @@ public class Tabuleiro {
     }
 
     // arvores - cidade2
+
+    for (int j = 6; j <= 10; j++) {
+        tabuleiro[68][j] = new Parede("\u001B[48;5;94m" + "🟫" + "\u001B[0m");
+    }
+
+    for (int j = 34; j <= 50; j++) {
+        tabuleiro[69][j] = new Parede("\u001B[48;5;94m" + "🟫" + "\u001B[0m");
+    }
+
+    for (int j = 38; j <= 40; j++) {
+        tabuleiro[43][j] = new Parede("\u001B[48;5;94m" + "🟫" + "\u001B[0m");
+    }
+
     for (int i = 78; i <= 79; i++) {
         for (int j = 24; j <= 38; j++) {
             tabuleiro[i][j] = new Parede("\u001B[42m"  + "🍀" + "\u001B[0m");
@@ -553,7 +994,98 @@ public class Tabuleiro {
         }
     }
 
+    for (int j = 63; j <= 68; j++) {
+        tabuleiro[44][j] = new Parede("\u001B[42m"  + "🍀" + "\u001B[0m");
+    }
+
+    for (int j = 81; j <= 85; j++) {
+        tabuleiro[44][j] = new Parede("\u001B[42m"  + "🍀" + "\u001B[0m");
+    }
+
+    for (int j = 84; j <= 85; j++) {
+        tabuleiro[45][j] = new Parede("\u001B[42m"  + "🍀" + "\u001B[0m");
+    }
+
+    tabuleiro[45][63] = new Parede("\u001B[42m"  + "🍀" + "\u001B[0m");
+    tabuleiro[46][85] = new Parede("\u001B[42m"  + "🍀" + "\u001B[0m");
+    tabuleiro[58][85] = new Parede("\u001B[42m"  + "🍀" + "\u001B[0m");
     
+    for (int j = 84; j <= 85; j++) {
+        tabuleiro[59][j] = new Parede("\u001B[42m"  + "🍀" + "\u001B[0m");
+    }
+
+    for (int j = 84; j <= 85; j++) {
+        tabuleiro[60][j] = new Parede("\u001B[42m"  + "🍀" + "\u001B[0m");
+    }
+
+    for (int j = 82; j <= 85; j++) {
+        tabuleiro[61][j] = new Parede("\u001B[42m"  + "🍀" + "\u001B[0m");
+    }
+
+    for (int j = 79; j <= 85; j++) {
+        tabuleiro[62][j] = new Parede("\u001B[42m"  + "🍀" + "\u001B[0m");
+    }
+
+    for (int j = 75; j <= 85; j++) {
+        tabuleiro[63][j] = new Parede("\u001B[42m"  + "🍀" + "\u001B[0m");
+    }
+
+    // montanha
+
+    for (int j = 46; j <= 50; j++) {
+        tabuleiro[j][63] = new Parede("\u001B[48;5;94m" + "🟫" + "\u001B[0m");
+    }
+
+    for (int i = 51; i <= 52; i++) {
+        for (int j = 63; j <= 82; j++) {
+            tabuleiro[i][j] = new Parede("\u001B[48;5;94m" + "🟫" + "\u001B[0m");
+        }
+    }
+
+    for (int i = 49; i <= 50; i++) {
+        for (int j = 81; j <= 85; j++) {
+            tabuleiro[i][j] = new Parede("\u001B[48;5;94m" + "🟫" + "\u001B[0m");
+        }
+    }
+
+    for (int j = 75; j <= 80; j++) {
+        tabuleiro[45][j] = new Parede("\u001B[48;5;94m" + "🟫" + "\u001B[0m");
+        tabuleiro[48][j] = new Parede("\u001B[48;5;94m" + "🟫" + "\u001B[0m");
+    }
+
+    tabuleiro[46][75] = new Parede("\u001B[48;5;94m" + "🟫" + "\u001B[0m");
+    tabuleiro[47][75] = new Parede("\u001B[48;5;94m" + "🟫" + "\u001B[0m");
+    tabuleiro[46][80] = new Parede("\u001B[48;5;94m" + "🟫" + "\u001B[0m");
+    tabuleiro[47][80] = new Parede("\u001B[48;5;94m" + "🟫" + "\u001B[0m");
+
+    for (int i = 53; i <= 54; i++) {
+        for (int j = 67; j <= 70; j++) {
+            tabuleiro[i][j] = new Parede("\u001B[48;5;94m" + "🟫" + "\u001B[0m");
+        }
+        for (int j = 73; j <= 76; j++) {
+            tabuleiro[i][j] = new Parede("\u001B[48;5;94m" + "🟫" + "\u001B[0m");
+        }
+    }
+
+    for (int j = 65; j <= 72; j++) {
+        tabuleiro[57][j] = new Caminho3();
+    }
+
+    for (int i = 55; i <= 56; i++) {
+        for (int j = 71; j <= 72; j++) {
+            tabuleiro[i][j] = new Caminho3();
+        }
+    }
+
+    for (int j = 71; j <= 72; j++) {
+        tabuleiro[53][j] = new Parede("\u001B[40m" + "⬛" + "\u001B[0m");
+    }
+
+    tabuleiro[54][71] = new PortaPreta(15, 25);
+    tabuleiro[54][72] = new PortaPreta(15, 25);
+
+
+
         // pokemons
         Pokemon bullbassaur = new Bulbassaur();
         Pokemon squirtle = new Squirtle();
